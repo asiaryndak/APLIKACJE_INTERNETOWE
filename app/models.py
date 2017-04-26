7027494efb1ee16d9f85f69fc17fae32a6abd0e2
Paweł Wójcik <pwojcik.main@gmail.com>
2017-04-26 23:00:03 +0200

    Dołączenie bazy danych, Utworzenie modelu przechowującego zapisane pliki, Stworzenie prostego widoku strony głównej, Wyłączenie domyślnego panelu administracyjnego.

--- app/models.py
+++ app/models.py
@@ -3,4 +3,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class File(models.Model):
+    name = models.CharField(max_length=32)
+    created = models.DateTimeField('date published')
