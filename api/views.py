3abff3a02a1804f813f319591ae02acddfa5812e
Paweł Wójcik <pwojcik.main@gmail.com>
2017-05-02 00:59:40 +0200

    Zapisywanie plików

--- api/views.py
+++ api/views.py
@@ -3,6 +3,9 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
+from .forms import UploadFileForm
+import uuid
+
 
 @require_http_methods(["GET"])
 def index(request):
@@ -16,14 +19,30 @@ def index(request):
 @csrf_exempt
 @require_http_methods(["POST"])
 def upload(request):
+    uid = ''
+    success = False
+
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            uid = saveFile(request.FILES['file'])
+            success = True
+
     data = {
-        'success'   :   True,
+        'success'   :   success,
         'errors'    :   {},
         'data'      :   {
             'files' : {
-                'name'      :   '84e5bccddbd31e3040a19ff5f43826e7',
+                'name'      :   uid,
                 'created'   :   '2017-04-27 18:30:34'
             }
         }
     }
     return JsonResponse(data)
+
+def saveFile(file):
+    uid = uuid.uuid4().hex
+    with open('upload/' + uid, 'wb+') as destination:
+        for chunk in file.chunks():
+            destination.write(chunk)
+    return uid
