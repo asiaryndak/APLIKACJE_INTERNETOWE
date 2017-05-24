ecb9f5650f8bfb3257204fe9c96253f886b9f3be
Paweł Wójcik <pwojcik.main@gmail.com>
2017-05-24 19:41:09 +0200

    FIX. Naprawienie katalogu static.

--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@
 from django.http import JsonResponse
 from django.views.decorators.http import require_http_methods
 from django.views.decorators.csrf import csrf_exempt
-from .forms import UploadFileForm
+from forms import UploadFileForm
 import uuid
 
 
--- app/urls.py
+++ app/urls.py
@@ -6,9 +6,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-
-    # (r'^media/(?P</img>.*)$', 'django.views.static.serve',{'document_root': app/project/settings.MEDIA_ROOT})
-
-static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-# static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 ]
