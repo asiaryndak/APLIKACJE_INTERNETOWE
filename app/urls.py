f54932aa49c707838d1c5367a3c3688513f1d99f
Wojciech Stracilo <wojciech.stracilo@gmail.com>
2017-05-24 18:41:25 +0200

    new gui

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,14 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
 
+from project import settings
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+
+    # (r'^media/(?P</img>.*)$', 'django.views.static.serve',{'document_root': app/project/settings.MEDIA_ROOT})
+
+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+# static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 ]
--- project/settings.py
+++ project/settings.py
@@ -120,4 +120,23 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR),"static")
+MEDIA_ROOT =  os.path.join(os.path.dirname(BASE_DIR),"media")
+MEDIA_URL = '/media/'
+
+
+
+# STATIC_URL = '/static/'
+#
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
+
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static"),
+#     '/app/templates/app/img/',
+#]
